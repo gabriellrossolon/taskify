cf24c4fd08641e47fb45949ceb96cca220be6d6c
Gabriell <gabrirossolon@yahoo.com.br>
2025-05-27 17:04:33 -0400

    retirada do docker (momentanea)

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02540bed84dd4714316cc739fed6168dd27e27fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f168072e7df6b3a67f8c88f5c476da6178a96e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
